595abba233373475254b39ff98cdf217b89d30d0
jrtweedle1 <tweedle@google.com>
2024-04-02 12:02:45 -0500

    Implemented 4 space indentation.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 function App() {
-    return (
-    <>
-      <Navbar></Navbar>
-      <Home></Home>
-    </>);
+	return (
+		<>
+			<Navbar></Navbar>
+			<Home></Home>
+		</>
+	);
 }
 
 export default App;
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,34 +1,44 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, createContext } from "react";
 import Card from "./Card";
 
 function Home() {
-  const [allPokemon, setAllPokemon] = useState([]);
-  useEffect(() => {
-    fetchAllPokemon();
-  }, []);
+	const [allPokemon, setAllPokemon] = useState([]);
+	const [squad, setSquad] = useState([]);
 
-  function fetchAllPokemon() {
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
-      .then((response) => {
-        return response.json();
-      })
-      .then((allPokemon) => {
-        setAllPokemon(allPokemon.results);
-      });
-  }
+	useEffect(() => {
+		fetchAllPokemon();
+	}, []);
 
-  return (
-    <div>
-      <h1>Home</h1>
-      <h2>My Squad</h2>
-      <h2>Available Pokemon</h2>
-      <div>
-        {allPokemon.map(singlePokemon => {
-            return <Card key={singlePokemon.name} pokemon={singlePokemon}></Card>
-        })}
-      </div>
-    </div>
-  );
+	function fetchAllPokemon() {
+		fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+			.then((response) => {
+				return response.json();
+			})
+			.then((allPokemon) => {
+				setAllPokemon(allPokemon.results);
+			});
+	}
+
+	return (
+		<div>
+			<h1 className="text-center">Home</h1>
+			<h2 className="text-center">My Squad</h2>
+			<h2 className="text-center">Available Pokemon</h2>
+			<div class="container text-center">
+				<div class="row">
+					{allPokemon.map((singlePokemon) => {
+						return (
+							<Card
+								key={singlePokemon.name}
+								pokemon={singlePokemon}
+							></Card>
+						);
+					})}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Home;
+export const PokemonContext = createContext([]);
